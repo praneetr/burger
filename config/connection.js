cabf9fe162dd0c529354218cabc1e8c3d5b1c2fb
praneetr <praneet_row@yahoo.com>
2018-03-03 12:59:42 -0800

    update_connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 var mysql = require('mysql');
+var connectioninfo = {
+	host: 'gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	username: 'aeebvl78odlutng6',
+	password: 'ds22p4nnv5qv2rjs'
+}
 // var connection;
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection("mysql://aeebvl78odlutng6:ds22p4nnv5qv2rjs@gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rfwlp5w47f23yfk3");
 
 connection.connect();
 
